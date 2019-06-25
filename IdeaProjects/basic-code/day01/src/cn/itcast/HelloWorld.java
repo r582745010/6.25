a898400dc0def15c16edc9222c64b6663b3dce03
a5827 <582745010@qq.com>
2019-06-25 23:17:34 +0800

    修改一次

--- IdeaProjects/basic-code/day01/src/cn/itcast/HelloWorld.java
+++ IdeaProjects/basic-code/day01/src/cn/itcast/HelloWorld.java
@@ -3,9 +3,15 @@ package cn.itcast;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,World///");
+        System.out.println(222);
+        System.out.println(222);
+        System.out.println(222);
+        System.out.println(222);
+
     }
     public void s(){
         System.out.println(11);
         System.out.println(11);
+        System.out.println(222);
     }
 }
